4c19e450b2ebdd776ddb9e26f20bc535fdd72cc3
Clement <clement.matheron@gmail.com>
2016-03-15 08:24:16 +0000

    first commit

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
 'use strict';
 var express = require('express');
-var routes = require('./app/routes/index.js');
-var mongo = require('mongodb');
 
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' }).single('userFile')
 require('dotenv').config();
 var app = express();
 
-mongo.MongoClient.connect(process.env.MONGOLAB_URI || 'mongodb://localhost:27017/clementinejs', function (err, db) {
+app.use('/public', express.static(process.cwd() + '/public'));
 
-    if (err) {
-        throw new Error('Database failed to connect!');
-    } else {
-        console.log('MongoDB successfully connected on port 27017.');
-    }
-
-    app.use('/public', express.static(process.cwd() + '/public'));
-    app.use('/common', express.static(process.cwd() + '/app/common'));
-    app.use('/controllers', express.static(process.cwd() + '/app/controllers'));
-
-    routes(app, db);
-
-    var port = process.env.PORT || 8080;
-	app.listen(port,  function () {
-		console.log('Node.js listening on port ' + port + '...');
-	});
-
+app.get('/', function(req,res){
+	res.sendFile(process.cwd()+'/public/index.html');
 });
+
+app.post('/',function(req,res){
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("Size :" + req.file.size + " bytes");
+    });
+});
+
+
+var port = process.env.PORT || 8080;
+app.listen(port,  function () {
+	console.log('Node.js listening on port ' + port + '...');
+});
+
+
 
